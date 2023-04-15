3b4f3795289a59a5f1ee0a65a10793c72ed2c1f0
ElectricGun <68138799+ElectricGun@users.noreply.github.com>
2023-04-15 17:29:24 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,5 @@
 const Commands = require("message-executor")
 
-Events.run(Trigger.update, () => {
-    if(!Vars.state.menu & !Vars.state.paused) {
-        Commands.readMessages()
-    }
-})
-
 Events.on(ClientLoadEvent, () => {
     /*Commands.addCommand("js",
     [function jsfunc(messageBlock, javascript){    //TO FIX unterminated loops will crash the game

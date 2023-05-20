5d29d9369cc6c9bde51b8d3c65f35aad0957d0e7
ElectricGun <68138799+ElectricGun@users.noreply.github.com>
2023-05-20 12:50:03 +0700

    aa

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,5 @@
 const Commands = require("message-executor")
 
 Events.on(ClientLoadEvent, () => {
-    /*Commands.addCommand("js",
-    [function jsfunc(messageBlock, javascript){    //TO FIX unterminated loops will crash the game
-        try{
-
-            eval(javascript)
-            print(messageBlock+ " Executed: ")
-
-        } catch(exception) {
-            if((exception instanceof InternalError)) {
-
-                print(messageBlock+ "[ERROR] Cannot evaluate functions within message block")
-
-            } else {
-
-                print(messageBlock+ " "+ exception)
-            }
-        } return null
-    }, 
-    ["string"]]
-    )
-    */
+    
 })
